b319942a6c8bfe61d281acacf511ba1481782946
Kumassy <Kumassy@users.noreply.github.com>
2016-03-14 19:43:46 +0900

    Added files via upload

---
+++ lib/mc_dot_art_maker.rb
@@ -0,0 +1,19 @@
+require 'singleton'
+
+require 'nbt_utils'
+require 'color-rgb'
+require 'rmagick'
+
+# require_relative 'mc_dot_art_maker/block'
+# require_relative 'mc_dot_art_maker/block_list'
+# require_relative 'mc_dot_art_maker/cell'
+# require_relative 'mc_dot_art_maker/table'
+# require_relative 'mc_dot_art_maker/schematic_helper'
+# require_relative 'mc_dot_art_maker/utils'
+
+require File.expand_path('../mc_dot_art_maker/block', __FILE__)
+require File.expand_path('../mc_dot_art_maker/block_list', __FILE__)
+require File.expand_path('../mc_dot_art_maker/cell', __FILE__)
+require File.expand_path('../mc_dot_art_maker/table', __FILE__)
+require File.expand_path('../mc_dot_art_maker/schematic_helper', __FILE__)
+require File.expand_path('../mc_dot_art_maker/utils', __FILE__)

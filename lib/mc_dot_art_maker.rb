8784cb3bb797e7cc30636d60c484bb3eb95012bd
Kumassy <kumassyii@gmail.com>
2016-03-15 00:03:29 +0900

    change file path

--- lib/mc_dot_art_maker.rb
+++ lib/mc_dot_art_maker.rb
@@ -4,9 +4,9 @@ require 'nbt_utils'
 require 'color-rgb'
 require 'rmagick'
 
-require File.expand_path('../mc_dot_art_maker/block', __FILE__)
-require File.expand_path('../mc_dot_art_maker/block_list', __FILE__)
-require File.expand_path('../mc_dot_art_maker/cell', __FILE__)
-require File.expand_path('../mc_dot_art_maker/table', __FILE__)
-require File.expand_path('../mc_dot_art_maker/schematic_helper', __FILE__)
-require File.expand_path('../mc_dot_art_maker/utils', __FILE__)
+require File.expand_path('../mc_dot_art_maker/block.rb', __FILE__)
+require File.expand_path('../mc_dot_art_maker/block_list.rb', __FILE__)
+require File.expand_path('../mc_dot_art_maker/cell.rb', __FILE__)
+require File.expand_path('../mc_dot_art_maker/table.rb', __FILE__)
+require File.expand_path('../mc_dot_art_maker/schematic_helper.rb', __FILE__)
+require File.expand_path('../mc_dot_art_maker/utils.rb', __FILE__)

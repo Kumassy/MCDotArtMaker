a5b17de60654ad6aefc9cad39637dee9ab18d652
Kumassy <kumassyii@gmail.com>
2016-03-14 22:03:27 +0900

    modify example

--- example/make_dot_art/make_dot_art.rb
+++ example/make_dot_art/make_dot_art.rb
@@ -1,13 +1,5 @@
 require_relative '../../lib/mc_dot_art_maker'
 
-# filename = 'ankou_kaijin.png'
-# table = MCDotArtMaker::Table.new(filename,1)
-
-# table.mosaic_image.write 'mozaic.png'
-# table.texture_image.write 'ankou_texture.png'
-# table.write_schematic 'ankou.schematic'
-# table.calculate
-
 Dir::entries("src").reject{|e| e[0]=='.'}.each do |entry|
   table = MCDotArtMaker::Table.new("src/#{entry}")
   base = entry.split('.').first
